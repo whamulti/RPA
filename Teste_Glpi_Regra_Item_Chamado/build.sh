5036676ef03faba9f057fca22441fc7fae716297
Ricardo <ricardo98349@outlook.com>
2026-02-10 16:52:43 -0300

    ok

--- Teste_Glpi_Regra_Item_Chamado/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-zip -r "Teste.zip" * -x "Teste.zip" -x "*.xaml" -x "*.jproj"
--- Teste_Glpi_Regra_Item_Chamado/requirements.bat
+++
@@ -1,4 +0,0 @@
-﻿@ECHO OFF
-autoflake --in-place --remove-unused-variables --remove-all-unused-imports --recursive ./
-pipreqs ./ --force
-
