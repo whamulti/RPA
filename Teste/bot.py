6eca8a2238640a305976f9d942f873edb8d7a197
Ricardo <ricardo98349@outlook.com>
2026-04-25 14:00:26 -0300

    ok

--- Teste/bot.py
+++ Teste/bot.py
@@ -1,5 +1,3 @@
-# Community Version: This license is intended for educational and tool evaluation purposes.
-
 from botcity.web.browsers.chrome import default_options
 from webdriver_manager.chrome import ChromeDriverManager
 from botcity.web import *
